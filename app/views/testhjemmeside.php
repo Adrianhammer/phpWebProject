14c36d9ede00df258c319621909a20f53418fa8b
Adel <adelh@uia.no>
2021-11-28 20:34:34 +0100

    Lagt til login og test sider

---
+++ app/views/testhjemmeside.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    if(isset($_SESSION["Fornavn"]) !== true) {
+        header("Location: ../controllers/login.php");
+        exit();
+    } else{
+        echo "<h2>Velkommen til medlemssystemets hjemmeside</h2><br><br>";
+        echo "<a href='testMinSide.php'>Gå til Min Side<br><br></a>";
+    }
+
+    if (isset($_REQUEST['loggut'])) {
+        session_start();
+        session_unset();
+        session_destroy();
+        header("Location: ../controllers/login.php");
+        exit();
+    }
+?>
+
+<form action ="<?php echo $_SERVER['PHP_SELF'];?>" method="request">
+<input type="submit" name="loggut" value="Logg Ut">
+</form>
