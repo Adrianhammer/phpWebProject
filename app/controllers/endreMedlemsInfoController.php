c0558d5ba7db585fb16206141466fd00d014155d
Adrian <adrhmmr@gmail.com>
2021-12-02 12:14:24 +0100

    kommentarer

--- app/controllers/endreMedlemsInfoController.php
+++ app/controllers/endreMedlemsInfoController.php
@@ -5,24 +5,39 @@ include ("../models/endreMedlemsInfoModels.php");
 
 echo "<br>Velg medlem</br>";
 
-if ($r_set=$conn->query($result)) {
+/* if ($r_set=$conn->query($result)) {
     echo"<select name=members class='form-control' style='width:200px;'>";
-
     while($row=$r_set->fetch_assoc()) {
-        
         echo "<option value=$row[ID]> ($row[ID]) $row[Fornavn] </option>";
-    
     }
-
     echo "</select>";
-
 } else {
     echo $conn->error;
+} */
+
+echo"<select name=members class='form-control' style='width:200px;'>";
+
+$sql = mysqli_query($conn, "SELECT ID, Fornavn FROM medlemmer");
+$row = mysqli_num_rows($sql);
+
+while($row = mysqli_fetch_array($sql)){
+    $id = $row['ID'];
+    $fnavn = $row['Fornavn'];
+    echo "<option value=$id> $id $fnavn </option>";
 }
+echo "</select>";
 
+//Hente ID som blir valgt fra dropdown
+//ID som blir valgt i dropdown skal bli satt som variabel
+//Må kanskje ha submit knapp 
+//1: Velg et medlem
+//2: Hvis submit knapp trykket
+//3: Medlemmet som er valgt blir puttet i en variabel
+//4: Variabel blir sendt til SQL query
+//ID variabel skal bli satt inn i SQL query
+//Formet vil da automatisk oppdatere seg med informasjon fra databasen etter hvilken ID som er valgt
 
-
-$result = mysqli_query($conn, "SELECT * FROM Medlemmer WHERE ID = 1");
+$result = mysqli_query($conn, "SELECT * FROM Medlemmer WHERE ID = $id");
 
     while($res = mysqli_fetch_array($result))
 {
