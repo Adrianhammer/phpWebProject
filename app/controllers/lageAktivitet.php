dd69b15ab3edb01b487a40629a3a4abc60a27603
Adel <adelh@uia.no>
2021-12-03 12:48:14 +0100

    Fikset session Include + endret layout til lageAktiviter pga Session() konflikt

--- app/controllers/lageAktivitet.php
+++ app/controllers/lageAktivitet.php
@@ -1,58 +1,3 @@
-<html>
-<head>
-<title>OPTION</title>
-</head>
-<body>
-    <form action="lageAktivitet.php" method="get">
-        Aktivitet: 
-        <br><select name="aktivitet">
-            <option value="select">--Velg Aktivitet--</option>
-            <option value="Fotball">Fotball</option>
-            <option value="Basketball">Basketball</option>
-            <option value="Gaming">Gaming</option>
-            <option value="Maling">Maling</option>
-        </select><br>
-
-        Ansvarlig: 
-        <br><select name="ansvarlig">
-            <option value="select">--Velg Ansvarlig--</option>
-            <option value="Ola Nordmann">Ola Nordmann</option>
-            <option value="Største Sjefen">Største Sjefen</option>
-            <option value="James Bond">James Bond</option>
-            <option value="Harry Potter">Harry Potter</option>
-        </select><br>
-
-        Fra: 
-        <br><select name="timeFrom">
-        <option value="select">--Velg Tid--</option>
-        <?php 
-        for($hours=0; $hours<24; $hours++) // the interval for hours is '1'
-        for($mins=0; $mins<60; $mins+=30) // the interval for mins is '30'
-            echo '<option>'.str_pad($hours,2,'0',STR_PAD_LEFT).':'
-                           .str_pad($mins,2,'0',STR_PAD_LEFT).'</option>';
-        
-        ?>    
-        </select><br>
-
-        Til: 
-        <br><select name="timeTo">
-        <option value="select">--Velg Tid--</option>
-        <?php 
-        for($hours=0; $hours<24; $hours++) // the interval for hours is '1'
-        for($mins=0; $mins<60; $mins+=30) // the interval for mins is '30'
-            echo '<option>'.str_pad($hours,2,'0',STR_PAD_LEFT).':'
-                           .str_pad($mins,2,'0',STR_PAD_LEFT).'</option>';
-        ?>    
-        </select><br>
-
-        Dato: 
-        <br><input type="date" name="fromDate" value=" " required><br> 
-        
-        <br><input type="submit" name="submit" value="Insert">
-    </form>
-</body>
-</html>
-
 <?php
 
 include "../../includes/includeDB.php";
@@ -140,4 +85,61 @@ $stmt->close();
 $conn->close();
     }
 }
+unset($stmt);
 ?>
+
+<html>
+<head>
+<title>OPTION</title>
+</head>
+<body>
+    <form action="lageAktivitet.php" method="get">
+        Aktivitet: 
+        <br><select name="aktivitet">
+            <option value="select" disable selected>--Velg Aktivitet--</option>
+            <option value="Fotball">Fotball</option required>
+            <option value="Basketball">Basketball</option required>
+            <option value="Gaming">Gaming</option required>
+            <option value="Maling">Maling</option required>
+        </select><br>
+
+        Ansvarlig: 
+        <br><select name="ansvarlig">
+            <option value="select" disable selected>--Velg Ansvarlig--</option>
+            <option value="Ola Nordmann">Ola Nordmann</option required>
+            <option value="Største Sjefen">Største Sjefen</option required>
+            <option value="James Bond">James Bond</option required>
+            <option value="Harry Potter">Harry Potter</option required>
+        </select><br>
+
+        Fra: 
+        <br><select name="timeFrom">
+        <option value="select" disable selected>--Velg Tid--</option>
+        <?php 
+        for($hours=0; $hours<24; $hours++) // the interval for hours is '1'
+        for($mins=0; $mins<60; $mins+=30) // the interval for mins is '30'
+            echo '<option>'.str_pad($hours,2,'0',STR_PAD_LEFT).':'
+                           .str_pad($mins,2,'0',STR_PAD_LEFT).'</option required>';
+        
+        ?>    
+        </select><br>
+
+        Til: 
+        <br><select name="timeTo">
+        <option value="select" disable selected>--Velg Tid--</option>
+        <?php 
+        for($hours=0; $hours<24; $hours++) // the interval for hours is '1'
+        for($mins=0; $mins<60; $mins+=30) // the interval for mins is '30'
+            echo '<option>'.str_pad($hours,2,'0',STR_PAD_LEFT).':'
+                           .str_pad($mins,2,'0',STR_PAD_LEFT).'</option required>';
+        ?>    
+        </select><br>
+
+        Dato: 
+        <br><input type="date" name="fromDate" value=" " required><br> 
+        
+        <br><input type="submit" name="submit" value="Insert">
+    </form>
+    <a href="../views/testhjemmeside.php"><button>Tilbake til hjemmeside</button></a>
+</body>
+</html>
