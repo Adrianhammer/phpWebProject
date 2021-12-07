76bd195134ee661ad1ef536384d499ae49e5fb1c
Adel <adelh@uia.no>
2021-12-07 20:20:23 +0100

    Fikset login slik at den verifiserer Brukernavn mot Fornavn

--- app/views/registrereMedlemViews.php
+++ app/views/registrereMedlemViews.php
@@ -1,5 +1,5 @@
 <?php
-//include ("../../includes/session.php");
+include ("../../includes/session.php");
 include ("../controllers/registrereMedlemControllers.php");
 include ("../../includes/navbar.php");
 include ("../../includes/Footer.php");
