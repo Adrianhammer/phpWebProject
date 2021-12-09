ffca78e745c2313e951753ac99025b82b44dd971
Adel <adelh@uia.no>
2021-12-09 11:23:38 +0100

    Fikset Required

--- app/views/aktivitetViews.php
+++ app/views/aktivitetViews.php
@@ -15,43 +15,43 @@ include ("../../includes/includeDB.php");
     
     <form action="../controllers/lageAktivitet.php" method="get">
         Aktivitet: 
-        <br><select name="aktivitet">
-            <option value="select" disable selected>--Velg Aktivitet--</option>
-            <option value="Fotball">Fotball</option required>
-            <option value="Basketball">Basketball</option required>
-            <option value="Gaming">Gaming</option required>
-            <option value="Maling">Maling</option required>
+        <br><select name="aktivitet" required>
+            <option value="" disable selected>--Velg Aktivitet--</option>
+            <option value="Fotball">Fotball</option>
+            <option value="Basketball">Basketball</option>
+            <option value="Gaming">Gaming</option>
+            <option value="Maling">Maling</option>
         </select><br>
 
         Ansvarlig: 
-        <br><select name="ansvarlig">
-            <option value="select" disable selected>--Velg Ansvarlig--</option>
-            <option value="Ola Nordmann">Ola Nordmann</option required>
-            <option value="Største Sjefen">Største Sjefen</option required>
-            <option value="James Bond">James Bond</option required>
-            <option value="Harry Potter">Harry Potter</option required>
+        <br><select name="ansvarlig" required>
+            <option value="" disable selected>--Velg Ansvarlig--</option>
+            <option value="Ola Nordmann">Ola Nordmann</option>
+            <option value="Største Sjefen">Største Sjefen</option>
+            <option value="James Bond">James Bond</option>
+            <option value="Harry Potter">Harry Potter</option> 
         </select><br>
 
         Fra: 
-        <br><select name="timeFrom">
-        <option value="select" disable selected>--Velg Tid--</option>
+        <br><select name="timeFrom" required>
+        <option value="" disable selected>--Velg Tid--</option>
         <?php 
         for($hours=0; $hours<24; $hours++) // the interval for hours is '1'
         for($mins=0; $mins<60; $mins+=30) // the interval for mins is '30'
             echo '<option>'.str_pad($hours,2,'0',STR_PAD_LEFT).':'
-                           .str_pad($mins,2,'0',STR_PAD_LEFT).'</option required>';
+                           .str_pad($mins,2,'0',STR_PAD_LEFT).'</option>';
         
         ?>    
         </select><br>
 
         Til: 
-        <br><select name="timeTo">
-        <option value="select" disable selected>--Velg Tid--</option>
+        <br><select name="timeTo" required>
+        <option value="" disable selected>--Velg Tid--</option>
         <?php 
         for($hours=0; $hours<24; $hours++) // the interval for hours is '1'
         for($mins=0; $mins<60; $mins+=30) // the interval for mins is '30'
             echo '<option>'.str_pad($hours,2,'0',STR_PAD_LEFT).':'
-                           .str_pad($mins,2,'0',STR_PAD_LEFT).'</option required>';
+                           .str_pad($mins,2,'0',STR_PAD_LEFT).'</option>';
         ?>    
         </select><br>
 
